eb3aff326348cdee93fb2fbeb629a688b3c0fc9d
Olivia <olivia@olivias-mbp.myfiosgateway.com>
2021-03-03 15:22:51 -0500

    updated READme with links

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-graphQL link: http://localhost:4000/griefNapkin
+**This is the server of Grief Napkin for our react native app.**
+
+deployed graphQL playground link: https://grief-napkin-mobile.herokuapp.com/
+graphQL localhost link: http://localhost:4000/griefNapkin
+client repo: https://github.com/oliviakasmin/grief-napkin-mobile-app-client
+
+Technolgies Used:
+- Mongodb
+- Mongoose
+- GraphQL
+- Apollo
---
+++ sitedata.js
@@ -0,0 +1,23 @@
+const lossType = [
+    'partner',
+    'friend',
+    'parent',
+    'grandparent',
+    'sibling',
+    'twin',
+    'someone I loved',
+    'acquantaince',
+    'prefer not to answer',
+  ]
+
+  const lossReason = [
+    'cancer',
+    'teminal illness (not cancer)',
+    'accident',
+    'overdose',
+    'unexpected freak problem',
+    'murder',
+    'suicide',
+    'a different shitty reason',
+    'prefer not to answer',
+  ],
