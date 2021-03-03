facd0e86e9f464422d3f6be4093134cd5dd73e05
Olivia <olivia@olivias-mbp.myfiosgateway.com>
2021-03-03 15:11:49 -0500

    deployment complete

--- server/graphQL/index.js
+++ server/graphQL/index.js
@@ -2,14 +2,9 @@ const { ApolloServer } = require('apollo-server')
 const typeDefs = require('./types')
 const resolvers = require('./resolvers')
 
-// APOLLO_KEY = 'service:grief-napkin-mobile-app-hsj03v:4pbIxhb-oTdXQM5G-E5mGA'
-// APOLLO_GRAPH_VARIANT = 'current'
-// APOLLO_SCHEMA_REPORTING = true
-
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  //   playground: true,
   engine: {
     apiKey: 'service:grief-napkin-mobile-app-hsj03v:4pbIxhb-oTdXQM5G-E5mGA',
   },
