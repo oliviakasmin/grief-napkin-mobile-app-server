c86f5a0bf060a16bec487d737a25e98086007c37
Olivia <olivia@olivias-mbp.myfiosgateway.com>
2021-03-03 17:28:18 -0500

    added db data

--- server/graphQL/index.js
+++ server/graphQL/index.js
@@ -1,18 +1,19 @@
 const { ApolloServer } = require('apollo-server')
 const typeDefs = require('./types')
 const resolvers = require('./resolvers')
+const { APOLLO_KEY } = require('../../secrets')
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   engine: {
-    apiKey: 'service:grief-napkin-mobile-app-hsj03v:4pbIxhb-oTdXQM5G-E5mGA',
+    apiKey: APOLLO_KEY,
   },
   introspection: true,
   // comment this in when deploying
-  // playground: {
-  //   endpoint: 'https://grief-napkin-mobile.herokuapp.com/',
-  // },
+  playground: {
+    endpoint: 'https://grief-napkin-mobile.herokuapp.com/',
+  },
 })
 
 module.exports = { server }
